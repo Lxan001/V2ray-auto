e123ecb8ae7aa5d3d7244711bb9ed6e741913cb5
Lxan001 <64342034+Lxan001@users.noreply.github.com>
2020-04-27 09:13:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -922,15 +922,28 @@ try_enable_bbr() {
 }
 
 get_ip() {
-	ip=$(curl -s https://ipinfo.io/ip)
-	[[ -z $ip ]] && ip=$(curl -s https://api.ip.sb/ip)
-	[[ -z $ip ]] && ip=$(curl -s https://api.ipify.org)
-	[[ -z $ip ]] && ip=$(curl -s https://ip.seeip.org)
-	[[ -z $ip ]] && ip=$(curl -s https://ifconfig.co/ip)
-	[[ -z $ip ]] && ip=$(curl -s https://api.myip.com | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
-	[[ -z $ip ]] && ip=$(curl -s icanhazip.com)
-	[[ -z $ip ]] && ip=$(curl -s myip.ipip.net | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
-	[[ -z $ip ]] && echo -e "\n$red 这垃圾小鸡扔了吧！$none\n" && exit
+
+	ipv4=$(curl -4 -s ipv4.icanhazip.com)
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 https://api.ip.sb/ip)
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 https://api.ipify.org)
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 https://ip.seeip.org)
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 https://ifconfig.co/ip)
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 https://api.myip.com | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 icanhazip.com)
+	[[ -z $ipv4 ]] && ipv4=$(curl -s -4 myip.ipip.net | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
+	[[ -z $ipv4 ]] && echo -e "\n$red 这垃圾小鸡扔了吧！$none\n" 
+    
+    ipv6=$(curl -6 -s ipv6.icanhazip.com)
+	[[ -z $ipv6 ]] && ipv6=$(curl -s -6 https://api.ip.sb/ip)
+	[[ -z $ipv6 ]] && ipv6=$(curl -s -6 https://ip.seeip.org)
+	[[ -z $ipv6 ]] && ipv6=$(curl -s -6 https://ifconfig.co/ip)	
+	[[ -z $ipv6 ]] && ipv6=$(curl -s -6 icanhazip.com)
+	[[ -z $ipv6 ]] && echo -e "\n$red 这垃圾小鸡扔了吧！$none\n"
+	
+	if [ $ipv6 ]; then
+	ip=$ipv6 ##默认ipv6 edit by Scaleya
+	else ip=$ipv4
+	fi
 }
 
 error() {
